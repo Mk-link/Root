54a305c1f396acff936958aa546b0806aba24e58
Markus Klink <markus.klink1987@gmail.com>
2025-09-25 13:03:06 +0200

    Dokumente

---
+++ # start.py
@@ -0,0 +1,8 @@
+# start
+name = "Markus"
+country = "germany"
+age = 38
+result = ""
+result="Hello my name is " + name + ", I am from " + country + " and I am " + str(age) + " years old."
+print(result)
+#end
