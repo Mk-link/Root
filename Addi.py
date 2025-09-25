28a8e1648555608c65643835dbf5b0a787308819
Markus Klink <markus.klink1987@gmail.com>
2025-09-25 14:18:28 +0200

    Dokumente

--- Addi.py
+++ Addi.py
@@ -1,8 +1,20 @@
 #start
-print("Hello,my name is Addi,i add numbers for you.What is your first number?")
-num1 = int(input())
-print("Thank you.\What is your second number?")
-num2 = int(input())
+print("Hello,my name is Addi,i add numbers for you.\nWhat is your first number?")
+while True:
+    try:
+        num1 = int(input())
+        break
+    except ValueError:
+        print("Please enter a valid number.")
+
+print("Thank you.\nWhat is your second number?")
+while True:
+    try:
+        num2 = int(input())
+        break
+    except ValueError:
+        print("Please enter a valid number.")
+
 result = num1 + num2
-print("The result is " + str(result))
+print("Awesome! The result is " + str(result))
 #end
