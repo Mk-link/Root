10829c6fd2d8830d89229b66281a4a08b58f281b
Markus Klink <markus.klink1987@gmail.com>
2025-09-30 12:38:20 +0200

    Dokumente

--- Aufgabe1Datentypen.py
+++ Aufgabe1Datentypen.py
@@ -1,5 +1,10 @@
-int'25'
-int"Hallo Welt!"
-float'3.14159'
-bool'True'
-
+x = int('25')
+y = str("Hallo Welt!")
+z = float('3.14159')
+b = bool('True')  # Achtung: bool('True') ergibt True, aber bool('') ergibt False
+"""
+int('25') wandelt den String '25' in eine Ganzzahl um.
+str("Hallo Welt!") ist eigentlich schon ein String, aber so ist die Syntax korrekt.
+float('3.14159') wandelt den String in eine Fließkommazahl um.
+bool('True') ergibt True, weil jeder nicht-leere String in Python als True gilt.
+"""
