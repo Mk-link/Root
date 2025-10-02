0681ca4704072fc65c8fa2cbae2aae8b3333de59
Markus Klink <markus.klink1987@gmail.com>
2025-10-02 11:13:18 +0200

    Tag3

---
+++ ageMachine.py
@@ -0,0 +1,19 @@
+def ageMachine(age: int):
+    if age > 50:
+        print("Du bist alt geworden!")
+    else:
+        print("werd erwachsen.")
+    print("This is the age machine.")
+#Definition immer zuerst, dann Aufruf
+
+"""
+this function only accepts two integers and returns their sum as an integer
+and then returns it
+"""
+
+def addTwoNumbers(first_number: int, second_number: int)-> int:
+    return first_number + second_number
+
+
+ageMachine()
+
