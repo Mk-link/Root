27d1676cc97b96103cf0105b8c5150c4fec839a1
Markus Klink <markus.klink1987@gmail.com>
2025-10-10 13:11:16 +0200

    xy

---
+++ ceasar.py
@@ -0,0 +1,13 @@
+#implementation of ceasar cipher
+def encrypt()->str:
+    return "it is encrypted"
+
+def decrypt()->str:
+    return "it is decrypted"
+
+def main():
+    print("This is ceasar cipher")
+
+if __name__ == "__main__":
+    main()
+
