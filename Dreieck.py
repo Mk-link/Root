8f13e589f2d85411f5f6be5803b6d58f4c947695
Markus Klink <markus.klink1987@gmail.com>
2025-11-04 11:35:12 +0100

    root

--- Dreieck.py
+++
@@ -1,8 +0,0 @@
-def dreieck(n):
-    """Return a string that represents a triangle of height n using asterisks."""
-    result = ""
-    for i in range(1, n + 1):
-        result += '*' * i + '\n'
-    return result  
-n = int(input("Enter the height of the triangle: "))
-print(dreieck(n))
